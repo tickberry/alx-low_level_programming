d3be69b30db2997126c7d449b432fe22bf720a05
tickberry <tickberry@hotmail.com>
2022-06-03 06:37:59 -0700

    a c program that assigns random numbers to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - going to chekc the condtion
  *
@@ -21,7 +22,7 @@ int main(void)
 	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}else
+	} else
 	{
 		printf("%d is negative\n", n);
 	}
