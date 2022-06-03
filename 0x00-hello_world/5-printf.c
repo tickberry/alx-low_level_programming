dfe42248940d26f8052eb29898983d15f76dfd34
tickberry <tickberry@hotmail.com>
2022-06-02 19:32:39 -0700

    a c program that prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - prints texts
  *description: "prints the outcome is a piece of art"
- * return: always 0 (success)
+ * return: "always 0 (success)"
  */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
