91d0dd19db9f86ac708a70a98d64adbc831d6b9a
tickberry <tickberry@hotmail.com>
2022-06-06 04:53:47 -0700

    a program that prints the alphabets in lowercase and uppercase followed by new line

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,8 @@ int main(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		printf("%c ", i);
+		printf("%c", i);
 	for (i = 'A'; i <= 'Z'; i++)
-		printf("%c ", i);
+		printf("%c", i);
 	return (0);
 }
