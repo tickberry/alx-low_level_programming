ec12c45a62737ba31e19165808f7eb42b0d27851
tickberry <tickberry@hotmail.com>
2022-06-06 07:08:43 -0700

    a program that prints the alphabets in lowercase and uppercase followed by new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+/**
+ * main entry point
+ *
+ * Description: this code that print exception q & e
+ *
+ * Results : 0  as value
+ *
+ */
+
+int main(void)
+{
+	char i
+	for (i = 'a'; i <= 'z'; i++)
+		if (i == 'e' && i == 'q')
+			printf("%c", "grep[eq]", i);
+}
