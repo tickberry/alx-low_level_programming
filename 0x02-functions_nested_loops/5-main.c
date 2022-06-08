c36f61bbe1d2f2ecdd530a6eafdbeb1992e18c6b
tickberry <tickberry@hotmail.com>
2022-06-08 08:21:37 -0700

    a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry  check code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+}
